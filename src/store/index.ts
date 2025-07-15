f8bba40ae1ffeee02b4c53ff2d044a337fa75221
Fernando Silva <artfernando.carlos@gmail.com>
2025-07-15 17:43:20 -0300

    atualizando explicação do store Reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,5 +11,7 @@ const store = configureStore({
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
+//- Atualização automática: Se você adicionar ou remover reducers do combineReducers, o tipo de RootReducer se adapta sem precisar mexer no código.
+// Essa tipagem é automática e inferida pelo TypeScript.
 
 export default store
